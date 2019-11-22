40c56213d3754119690ad731d411ba076ec4c2c1
Can <37780414+ccevik48@users.noreply.github.com>
2019-11-22 15:19:23 -0500

    Main cs file

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MajorityElement
+{
+    class Program
+    {
+        public static int majEle(int[] a)
+        {
+            int count = 1;
+            int currMaxCnt = 0;
+            int currMaxEle = a[0];
+            for(int i = 0; i < a.Length - 1; i++)
+            {
+                if(a[i] == a[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if(count > currMaxCnt)
+                {
+                    currMaxCnt = count;
+                    currMaxEle = a[i];
+                }
+            }
+            return currMaxEle;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 2, 2, 2, 7, 9, 1, 2 };
+            Console.WriteLine(majEle(arr));
+        }
+    }
+}
